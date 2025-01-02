6d6dd33ea6215dfcf83683899bf222313f0c926f
ishaanJ91 <jaini@tcd.ie>
2025-01-02 22:37:34 +0400

    vite.config.ts updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Keep "/" for root deployments; adjust for subdirectories
   build: {
     outDir: "dist",
   },
